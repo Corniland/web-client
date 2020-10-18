f373b1ba06013ef80a6a0963d92e5c9c74bbc045
Pierre Demessence <pierre.demessence@gmail.com>
2020-10-19 01:07:17 +0200

    Added working register, but need more validation and error messages.

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -4,30 +4,47 @@
       <v-flex xs12 sm10 md6 lg4 xl3>
         <h1>Register</h1>
 
-        <v-form v-model="valid" ref="form" lazy-validation class="pa-3 pt-4">
-          <v-text-field label="Username" v-model="email" prepend-icon="mdi-account" required />
-          <v-text-field label="Email" v-model="email" prepend-icon="mdi-email" required />
+        <v-form v-model="valid" ref="form" class="pa-3 pt-4">
+          <v-text-field
+            label="Username"
+            v-model="username"
+            prepend-icon="mdi-account"
+            required
+            :rules="[(v) => !!v || 'Username is required']"
+          />
+          <v-text-field
+            label="Email"
+            v-model="email"
+            autocomplete="email"
+            prepend-icon="mdi-email"
+            required
+            :rules="[(v) => !!v || 'Email is required', (v) => emailRegex.test(v) || 'Email must be valid']"
+          />
           <v-text-field
             label="Password"
             v-model="password"
+            autocomplete="new-password"
             :append-icon="hidePass ? 'mdi-eye' : 'mdi-eye-off'"
             @click:append="hidePass = !hidePass"
             :type="hidePass ? 'password' : 'text'"
             prepend-icon="mdi-lock"
             required
+            :rules="[(v) => !!v || 'Password is required']"
           />
           <v-text-field
-            label="Password Repeat"
-            v-model="password"
-            :append-icon="hidePass ? 'mdi-eye' : 'mdi-eye-off'"
-            @click:append="hidePass = !hidePass"
-            :type="hidePass ? 'password' : 'text'"
+            label="Password"
+            v-model="passwordRepeat"
+            autocomplete="new-password"
+            :append-icon="hidePassRepeat ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="hidePassRepeat = !hidePassRepeat"
+            :type="hidePassRepeat ? 'password' : 'text'"
             prepend-icon="mdi-lock"
             required
+            :rules="[(v) => !!v || 'Password is required']"
           />
         </v-form>
         <v-card-actions class="pa-3">
-          <v-btn @click="submit" :disabled="!valid" :loading="loadingBtn" block depressed>Register</v-btn>
+          <v-btn @click="submit" :disabled="!valid" :loading="loading" block depressed>Register</v-btn>
         </v-card-actions>
         <v-card-actions class="pa-3 justify-center">
           <v-btn to="/login" depressed text>Login</v-btn>
@@ -45,11 +62,40 @@ import { Component } from "vue-property-decorator";
   components: {},
 })
 export default class Home extends Vue {
-  valid = false;
-  hidePass = true;
+  email = "";
+  username = "";
+  password = "";
+  passwordRepeat = "";
 
-  submit(): void {
-    console.log("to");
+  hidePass = true;
+  hidePassRepeat = true;
+
+  valid = false;
+  loading = false;
+
+  // eslint-disable-next-line no-control-regex
+  emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+  $refs!: {
+    form: HTMLFormElement;
+  };
+
+  async submit(): Promise<void> {
+    if (this.$refs.form.validate()) {
+      this.loading = true;
+      try {
+        await this.$store.direct.dispatch.authentication.register({
+          username: this.username,
+          email: this.email,
+          password: this.password,
+        });
+        this.$router.push("/");
+      } catch (err) {
+        console.error(err);
+      } finally {
+        this.loading = false;
+      }
+    }
   }
 }
 </script>
