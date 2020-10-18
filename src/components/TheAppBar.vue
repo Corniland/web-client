3aa55f89a2df853e9885b547c9f3c6dbf92fbeab
Pierre Demessence <pierre.demessence@gmail.com>
2020-10-19 00:10:01 +0200

    AppBar logo now is a link to home

--- src/components/TheAppBar.vue
+++ src/components/TheAppBar.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app-bar app color="primary" dark>
-    <div class="d-flex align-center">
+    <router-link class="d-flex align-center" to="/">
       <v-img
         alt="Vuetify Logo"
         class="shrink mr-2"
@@ -18,7 +18,7 @@
         src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
         width="100"
       />
-    </div>
+    </router-link>
 
     <v-spacer></v-spacer>
 
